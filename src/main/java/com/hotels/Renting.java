7cd9f261d3ec3839040dc8fb580ca8f4b858be82
ZachKran <zachkranabetter@gmail.com>
2024-03-23 22:19:24 -0400

    added renting ☄️🏀

---
+++ src/main/java/com/hotels/Renting.java
@@ -0,0 +1,15 @@
+package com.hotels;
+
+public class Renting {
+    private String rentingID;
+    private String period;
+    private int numberOfRooms;
+
+
+    public Renting(String rentingID, String period, int numberOfRooms) {
+        this.rentingID = rentingID;
+        this.numberOfRooms = numberOfRooms;
+        this.period = period;
+
+    }
+}
