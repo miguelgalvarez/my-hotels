01dc3385a538c290dd97a38ad9ff8ce1eafbb4ea
miguelgalvarez <malva018@uottawa.ca>
2024-03-23 22:53:31 -0400

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,81 +1,112 @@
 -- ----------------------------
+-- Setting search-path
+-- ----------------------------
+set search_path = "my-hotels"
+
+-- ----------------------------
 -- Table structure for entities
 -- ----------------------------
 DROP TABLE IF EXISTS hotelchain;
 CREATE TABLE hotelchain (
-  ChainID serial PRIMARY KEY,
-  PhoneNumber varchar(45) NOT NULL,
-  NumberOfHotels INT NOT NULL,
-  hotelemail varchar(100) NOT NULL CHECK (hotelemail LIKE '_%@_%._%')
+	HotelChainID serial PRIMARY KEY,
+	HotelChainAddress varchar(45) NOT NULL,
+	PhoneNumber varchar(45) NOT NULL,
+	NumberOfHotels INT NOT NULL,
+	HotelChainEmail varchar(100) NOT NULL CHECK (HotelChainEmail LIKE '_%@_%._%')
 );
 
 DROP TABLE IF EXISTS hotel;
 CREATE TABLE hotel (
-  hotelid serial PRIMARY KEY,
-  address varchar(45) NOT NULL,
-  NumberOfRooms INT NOT NULL,
-  surname varchar(45) NOT NULL,
-  hotelemail varchar(100) NOT NULL CHECK (hotelemail LIKE '_%@_%._%')
+	HotelID serial PRIMARY KEY,
+	RoomID serial REFERENCES room(RoomID),
+	HotelChainID serial REFERENCES hotelchain(HotelChainID),
+	Address varchar(45) NOT NULL,
+	NumberOfRooms INT NOT NULL,
+	Surname varchar(45) NOT NULL,
+	HotelEmail varchar(100) NOT NULL CHECK (HotelEmail LIKE '_%@_%._%'),
+	Rating double precision NOT NULL
 );
 
 DROP TABLE IF EXISTS room;
 CREATE TABLE room (
-  roomID serial PRIMARY KEY,
-  address varchar(45) NOT NULL,
-  roomView varchar(45) NOT NULL,
-  price varchar(45) NOT NULL,
-  capacity INT NOT NULL,
-  numberOfRooms INT NOT NULL,
-  surname varchar(45) NOT NULL,
-  customeremail varchar(100) NOT NULL CHECK (customeremail LIKE '_%@_%._%')
+	RoomID serial PRIMARY KEY,
+	Address varchar(45) NOT NULL,
+	RoomView varchar(45) NOT NULL,
+	Price varchar(45) NOT NULL,
+	Capacity INT NOT NULL,
+	NumberOfRooms INT NOT NULL,
+	Surname varchar(45) NOT NULL,
+	Damages TEXT,
+	RoomExtension varchar(45),
+	CustomerEmail varchar(100) NOT NULL CHECK (CustomerEmail LIKE '_%@_%._%')
 );
+
+DROP TABLE IF EXISTS amenity;
+CREATE TABLE amenity (
+	RoomAmenityID serial PRIMARY KEY,
+	RoomAmenityName varchar(50) NOT NULL
+)
+
+DROP TABLE IF EXISTS room_amenity;
+CREATE TABLE room_amenity (
+	RoomID INT,
+	RoomAmenityID INT, 
+  	PRIMARY KEY (RoomID, RoomAmenityID),
+  	FOREIGN KEY (RoomID) REFERENCES room(RoomID),
+  	FOREIGN KEY (RoomAmenityID) REFERENCES amenity(RoomAmenityID)
+)
 
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (
-  customerID serial PRIMARY KEY,
-  address varchar(45) NOT NULL,
-  FullName varchar(45) NOT NULL,
-  IDpresented varchar(45) NOT NULL,
-  capacity INT NOT NULL,
-  NumberOfRooms INT NOT NULL,
-  surname varchar(45) NOT NULL,
-  customeremail varchar(100) NOT NULL CHECK (customeremail LIKE '_%@_%._%')
+	CustomerID serial PRIMARY KEY,
+	Address varchar(45) NOT NULL,
+	FullName varchar(45) NOT NULL,
+	IDPresented varchar(45) NOT NULL,
+	Capacity INT NOT NULL,
+	NumberOfRooms INT NOT NULL,
+	Surname varchar(45) NOT NULL,
+	CustomerEmail varchar(100) NOT NULL CHECK (CustomerEmail LIKE '_%@_%._%')
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-  employeeID serial PRIMARY KEY,
-  address varchar(45) NOT NULL,
-  FullName varchar(45) NOT NULL,
-  SSN INT NOT NULL,
-  role varchar(45) NOT NULL,
-  NumberOfRooms INT NOT NULL
+	EmployeeID serial PRIMARY KEY,
+	HotelID serial REFERENCES employee(HotelID),
+	Address varchar(45) NOT NULL,
+	FullName varchar(45) NOT NULL,
+	SSN INT NOT NULL,
+	Role varchar(45) NOT NULL,
+	NumberOfRooms INT NOT NULL
 );
 
 DROP TABLE IF EXISTS booking;
 CREATE TABLE booking (
-  BookingID serial PRIMARY KEY,
-  PricePaid varchar(45) NOT NULL,
-  period varchar(45) NOT NULL,
-  NumberOfRooms INT NOT NULL
+	BookingID serial PRIMARY KEY,
+	RentingID serial REFERENCES renting(RentingID),
+	CustomerID serial REFERENCES customer(CustomerID),
+	PricePaid varchar(45) NOT NULL,
+	Period varchar(45) NOT NULL,
+	NumberOfRooms INT NOT NULL
 );
 
 DROP TABLE IF EXISTS renting;
 CREATE TABLE renting (
-  RentingID serial PRIMARY KEY,
-  period varchar(45) NOT NULL,
-  CustomerName varchar(45) NOT NULL,
-  NumberOfRooms INT NOT NULL
+	RentingID serial PRIMARY KEY,
+	CustomerID serial REFERENCES customer(CustomerID),
+	RoomID serial REFERENCES room(RoomID),
+	Period varchar(45) NOT NULL,
+	CustomerName varchar(45) NOT NULL,
+	NumberOfRooms INT NOT NULL
 );
 
 -- ----------------------------
 -- Records of students
 -- ----------------------------
-INSERT INTO hotelchain (PhoneNumber, NumberOfHotels, hotelemail)
+INSERT INTO hotelchain (PhoneNumber, NumberOfHotels, HotelChainEmail, HotelChainAddress)
 VALUES
-    ('1234567890', 8, 'chain1@gmail.com'),
-    ('2345678901', 9, 'chain1@gmail.com'),
-    ('3456789012', 10, 'chain1@gmail.com'),
-    ('4567890123', 11, 'chain1@gmail.com'),
-    ('5678901234', 12, 'chain1@gmail.com');
+    ('1234567890', 8, 'chain1@gmail.com','33 Palisade St'),
+    ('2345678901', 9, 'chain1@gmail.com','33 Palisade St'),
+    ('3456789012', 10, 'chain1@gmail.com','33 Palisade St'),
+    ('4567890123', 11, 'chain1@gmail.com','33 Palisade St'),
+    ('5678901234', 12, 'chain1@gmail.com','33 Palisade St');
 
