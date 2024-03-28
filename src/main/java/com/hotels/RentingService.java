7ee2d8efbb671ad6609e99577f1c4e465d34dcf7
miguelgalvarez <malva018@uottawa.ca>
2024-03-27 21:43:45 -0400

    Update RentingService.java

--- src/main/java/com/hotels/RentingService.java
+++ src/main/java/com/hotels/RentingService.java
@@ -3,7 +3,6 @@ package com.hotels;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class RentingService {
     public boolean bookingTOrenting(int roomID) {
